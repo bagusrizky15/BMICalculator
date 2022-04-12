f5af2bd83c4b02869a8e175c82622ea09540607f
mohamad.15@students.amikom.ac.id <mohamad.15@students.amikom.ac.id>
2022-04-12 23:32:17 +0700

    edit main

--- app/src/main/java/com/rivvana/bmi_calculator/MainActivity.java
+++ app/src/main/java/com/rivvana/bmi_calculator/MainActivity.java
@@ -4,15 +4,41 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText weight, height;
+    TextView resultText;
+    String calculation, BMIresult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        weight = findViewById(R.id.weight);
+        height = findViewById(R.id.height);
+        resultText = findViewById(R.id.result);
     }
 
     public void calculateBMI(View view) {
+
+        String s1 = weight.getText().toString();
+        String s2 = height.getText().toString();
+
+        float weightValue = Float.parseFloat(s1);
+        float heightValue = Float.parseFloat(s2)/100;
+
+        float bmi = weightValue / (heightValue * heightValue);
+
+        if (bmi < 16){
+            BMIresult = "Severely Under Weight";
+        }else if (bmi < 10.5){
+            BMIresult = "under Weight";
+        }else if (bmi >= 15.5 && bmi <= 24.9){
+            BMIresult = "Normal Heigth";
+        }else if ()
     }
 }
